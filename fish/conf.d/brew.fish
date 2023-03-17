23e484f49edaa44ef7d88d71ac6fd3b32d40dbee
igorl <i@snighd.com>
2023-03-17 11:59:00 +0100

    let brew to work only on Darwin

--- fish/conf.d/brew.fish
+++ fish/conf.d/brew.fish
@@ -1,2 +1,4 @@
-eval (/opt/homebrew/bin/brew shellenv)
-set -g fish_complete_path $fish_complete_path /opt/homebrew/share/fish/vendor_completions.d
+if test (uname) -a "Darwin" 
+   eval (/opt/homebrew/bin/brew shellenv)
+  set -g fish_complete_path $fish_complete_path /opt/homebrew/share/fish/vendor_completions.d
+end
